d58ada27ce6167f63b7ce4035e632bebf684e192
qvalentin <valentin.theodor@web.de>
2024-08-26 23:44:49 +0200

    test: mock tests

--- tests/helm-ls/helm-ls_spec.lua
+++ tests/helm-ls/helm-ls_spec.lua
@@ -2,7 +2,7 @@ local plugin = require("helm-ls")
 
 describe("setup", function()
   it("works with default", function()
-    assert("Hello" == "Hello!", "my first function with param = Hello!")
+    assert("Hello!" == "Hello!", "my first function with param = Hello!")
   end)
 
   it("works with custom var", function()
